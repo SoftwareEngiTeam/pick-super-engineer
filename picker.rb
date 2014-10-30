9ee70d8b1db26bf482207a599da5a7a79853cb87
saasbook <saasbook@saasbook.(none)>
2014-10-29 23:58:56 -0300

    edit featureC() for get input from command

--- picker.rb
+++ picker.rb
@@ -8,14 +8,17 @@ def pick_engineer
    engineer_name = engineers[Random.rand(0...engineers.size())]
 end
 
-def feature_C(cha)
+def feature_C
+   cha = ARGV.map {|i| "" + i.to_s + ""}.join(",")
+   print cha
    engineer = File.open('engineers.txt').read.split("\n").each do |line|
    #file = File.read('engineers.txt').each_line do |line| 
-     if line.include? "#{cha}"
+     if line.include? "#{cha.to_s}"
        puts "finding #{cha} --> #{line}"
      end
    end
 end
 
 puts pick_engineer #print engineer name
-feature_C("ong")
+feature_C
+
