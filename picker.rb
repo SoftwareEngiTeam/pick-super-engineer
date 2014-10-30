ebd78d267054c701814c53c2dae00e75c86ac1af
parichat <p.larwongsa@gmail.com>
2014-10-29 23:09:15 -0300

    featureB

--- picker.rb
+++ picker.rb
@@ -8,7 +8,12 @@ def pick_engineer
    engineer_name = engineers[Random.rand(0...engineers.size())]
 end
 
+def feature_b(pick_engineer)
+	index = pick_engineer.split #split name and lastname in array	
+	index[0]+" "+index[1].chr+"..." #show name and first character lastname and ...
+end
 
 if __FILE__ == $0
    puts pick_engineer #print engineer name
+   puts feature_b(pick_engineer) #show engineer name from feature_b
 end
